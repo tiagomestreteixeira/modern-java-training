8ee2b62460d5a4c173cdc7526c5021b503c17ef6
Tiago Mestre Teixeira <tam.teixeira@gmail.com>
2022-09-23 19:45:17 +0100

    Add Decorator Design Pattern

--- basicsstrong/src/main/java/com/designpatterns/decorator/DecoratorDesignPattern.java
+++ basicsstrong/src/main/java/com/designpatterns/decorator/DecoratorDesignPattern.java
@@ -1,7 +1,60 @@
 package com.designpatterns.decorator;
 
+
+interface Burger {
+    void makeBurger();
+}
+
+class PlainBurger implements Burger {
+    @Override
+    public void makeBurger() {
+        System.out.println("Plain Burger is ready.");
+    }
+}
+
+abstract class BurgerDecorator extends PlainBurger {
+    protected Burger burger;
+
+    public BurgerDecorator(Burger burger) {
+        this.burger = burger;
+    }
+
+    @Override
+    public void makeBurger() {
+        super.makeBurger();
+    }
+}
+
+class CheeseBurgerDecorator extends BurgerDecorator {
+    public CheeseBurgerDecorator(Burger burger) {
+        super(burger);
+    }
+
+    @Override
+    public void makeBurger() {
+        burger.makeBurger();
+        System.out.println(", cheese added.");
+    }
+}
+
+class BarbecueBurgerDecorator extends BurgerDecorator {
+    public BarbecueBurgerDecorator(Burger burger) {
+        super(burger);
+    }
+
+    @Override
+    public void makeBurger() {
+        burger.makeBurger();
+        System.out.println(", vegetables and cheese added.");
+    }
+}
+
 public class DecoratorDesignPattern {
     public static void main(String[] args) {
+        Burger burger = new PlainBurger();
+
+        new CheeseBurgerDecorator(burger).makeBurger();
+        new BarbecueBurgerDecorator(burger).makeBurger();
 
     }
 }
